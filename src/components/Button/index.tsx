51c75f22915100bae564e8cf0d9de429f8f831fa
Alvapng <nogueraalvaro33@gmail.com>
2026-09-06 17:45:45 -0300

    Subir proyecto React TP-2

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+import styles from "./Button.module.css";
+import styled from "styled-components";
+
+type BtnProps = {
+  isLoading: boolean;
+};
+const Btn = styled.button<BtnProps>`
+  background-color: ${(props) => (props.isLoading ? "gray" : "red")};
+  padding: 25px 30px;
+`;
+
+type Props = {
+  children: ReactNode;
+  isLoading?: boolean;
+  onClick: () => void;
+};
+
+console.log(styles);
+function Button({ children, isLoading, onClick }: Props) {
+  return (
+    <Btn onClick={onClick} disabled={isLoading} isLoading={isLoading}>
+      {isLoading ? "Cargando..." : children}
+    </Btn>
+  );
+}
+
+export default Button;
